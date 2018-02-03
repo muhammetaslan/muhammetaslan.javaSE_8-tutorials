5a2b7ddcbc056e1191921caf9ae598490194c59c
muhammetaslan <muhammet.aslan1995@gmail.com>
2018-02-03 21:36:39 +0300

    lets go

--- src/muhammetaslan/javase/DeclarationAndAccessControl/Declaration.java
+++ src/muhammetaslan/javase/DeclarationAndAccessControl/Declaration.java
@@ -3,7 +3,8 @@ package muhammetaslan.javase.DeclarationAndAccessControl;
 public class Declaration {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
 
 	}
 
