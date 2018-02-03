24b147157c95e9d3c5a0a885e2e78c4c879824b4
muhammetaslan <muhammet.aslan1995@gmail.com>
2018-02-03 21:21:01 +0300

    Declaration.java commiting

---
+++ src/muhammetaslan/javase/DeclarationAndAccessControl/Declaration.java
@@ -0,0 +1,10 @@
+package muhammetaslan.javase.DeclarationAndAccessControl;
+
+public class Declaration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
