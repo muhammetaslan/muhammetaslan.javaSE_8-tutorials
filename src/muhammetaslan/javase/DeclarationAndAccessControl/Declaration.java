c9917c3bbd20e03e54f4275a73412b10563d9990
muhammetaslan <muhammet.aslan1995@gmail.com>
2018-02-04 23:41:41 +0300

    declaration and identifier name standard

--- src/muhammetaslan/javase/DeclarationAndAccessControl/Declaration.java
+++ src/muhammetaslan/javase/DeclarationAndAccessControl/Declaration.java
@@ -1,11 +1,73 @@
 package muhammetaslan.javase.DeclarationAndAccessControl;
 
+import java.awt.List;
+
 public class Declaration {
 
+	int temp = 0;
+	int $aslan = 22;
+	String name = "muhammet";
+	double _pi = 3.14;
+	
+	
+
+	/*
+	rakam ve $,_ dýsndakaki karakter ile identifier ler basalmaz
+	int 2number ;
+	String /name;
+	 */
+
+
 	public static void main(String[] args) {
+
+		// javada metodlara , degiþkenlere , class'lara , interface'lere verilen isimlere 
+		// identifier denir.
+		// degisken isimleri $,_,ve karakterler ile baþlar sadece
+		// degiþken isimlerinde herhangi bir karakter sayýsý limiti yoktur
+
+		Car car1 = new Car();
 		
-		System.out.println("Hello world");
+		List car = car1.getCars();
+		
+		for (int i = 0; i < car.getItemCount(); i++) {
+			System.out.println(car.getItem(i));
+		}//end of for
+		
 
 	}
 
 }
+class Car{
+
+	private List carList = new List();
+
+	public List getCars(){
+
+		carList.add("Ferrari");
+		carList.add("Toyata");
+
+		return carList;
+	}
+
+	/*
+	 * not legal 
+	public void (removeVar{
+
+	}
+	 */
+}
+
+/*
+ * not lagal
+class 2Car{
+
+}
+ */
+
+/*
+ *	Bir .java belgesinde yalnýzca bir public class buluna bilir 
+ *
+public class Meyve{
+	
+}
+*/
---
+++ src/muhammetaslan/javase/DeclarationAndAccessControl/Values.java
@@ -0,0 +1,12 @@
+package muhammetaslan.javase.DeclarationAndAccessControl;
+
+public interface Values {
+	
+	/*
+	 * java da constant degerler static final olarak tanýmlanmalýdýr.
+	 * */
+	public static final double PI = 3.14;
+	public static final int R = 7;
+	public static final String name = "aslan";
+	
+}
