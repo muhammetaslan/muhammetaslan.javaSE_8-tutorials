8bd01d553b11d0387df1e46549b627c5ac3ebb95
muhammetaslan <muhammet.aslan1995@gmail.com>
2018-02-07 23:48:00 +0300

    Modified nonAccessModifier.java

--- src/muhammetaslan/javase/modifier/NonAccessModifier.java
+++ src/muhammetaslan/javase/modifier/NonAccessModifier.java
@@ -5,6 +5,11 @@ public class NonAccessModifier extends AbstractClass{
 
 	public static void main(String[] args) {
 		
+		A3 a3_obj = new A3();
+		
+		a3_obj.showA4();
+		a3_obj.showFinalClass();
+		
 		System.out.println("worked");
 	}
 
@@ -15,6 +20,7 @@ public class NonAccessModifier extends AbstractClass{
 
 }
 
+// A1 class ý abstract olan A2 class ýný extend etti ve yalnýzca abstract class lar override edildi.
 class A1 extends A2{
 
 	@Override
@@ -36,13 +42,104 @@ class A1 extends A2{
 abstract class A2{
 	
 	final int MAX_SALARY = 45000;
+	private int age;
+	protected String villageName;
+	
+	
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getVillageName() {
+		return villageName;
+	}
+	public void setVillageName(String villageName) {
+		this.villageName = villageName;
+	}
+	public int getMAX_SALARY() {
+		return MAX_SALARY;
+	}
+	
 	public abstract void getSalary();
 	public abstract void getMoney();
 	
 }
 
-final class A3{
+/*
+ * B4 final class olan A3 un subclass ý olmaz final classlara extend edilemez
+class B4 extends A3{
 	
-	 private static String NAME = "MUHAMMET";
+}
+*/
+
+class A4{
+	
+	int ssn;
+	private int age;
+	
+	
+	public int getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(int ssn) {
+		this.ssn = ssn;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void showA4() {
+		System.out.println("A4");
+	}
+}
+
+
+// ama final classlar baska classlara extend edip kalýtabilirler.
+final class A3 extends A4{
+	
+	
+	private static String NAME = "MUHAMMET";
+	protected String name;
+	public String surname;
+	
+	
 	 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public static String getNAME() {
+		return NAME;
+	}
+
+	public static void setNAME(String nAME) {
+		NAME = nAME;
+	}
+
+
+
+	public void showFinalClass() {
+		System.out.println("I am final class");
+	}
 }
