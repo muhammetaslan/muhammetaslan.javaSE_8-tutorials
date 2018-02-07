ad217607785cf7b7cbf61d5e8343a268ee145200
muhammetaslan <muhammet.aslan1995@gmail.com>
2018-02-07 22:26:38 +0300

    Abstract class implementation committed

---
+++ src/muhammetaslan/javase/modifier/AbstractClass.java
@@ -0,0 +1,55 @@
+package muhammetaslan.javase.modifier;
+
+//egerki abstract methodlar tanýmlamak istiyorsak onlarýn bulundugu class da abstract olmalýdýr.
+//eger ki bir class abstract bir class ý extend ederse içindeki abstract metodlarý override etmemizi isteyecektir
+public abstract class AbstractClass {
+	
+	/*
+	 * Illegal modifier for the field denemeAbstract; 
+	 * only public, protected, private, static, final, 
+	 * transient & volatile are permitted
+	
+	abstract int denemeAbstract=0;
+	
+	*/
+	
+	private int id;
+	private String comment;
+	private static final double PI = 3.14;
+	
+	// method tanýmýmýza absract degerini girdiðimizde methoda body {} kýsmýný eklemek zorunda degiliz
+	public abstract void showClassName();
+	
+	//abstract metodlarýn body kýsmý olmaz
+	/*
+	public abstract int calculateArea() {
+		
+		return 0;
+	}*/
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public static double getPi() {
+		return PI;
+	}
+	
+	
+}
+
+
+
+
--- src/muhammetaslan/javase/modifier/NonAccessModifier.java
+++ src/muhammetaslan/javase/modifier/NonAccessModifier.java
@@ -1,12 +1,48 @@
 package muhammetaslan.javase.modifier;
 
-public class NonAccessModifier {
+// abstract class ý extend ettiðimizde yalnýzca içinde abstract olarak tanýmlanmýs methodlarý kullanmamýz ister
+public class NonAccessModifier extends AbstractClass{
 
 	public static void main(String[] args) {
 		
+		System.out.println("worked");
+	}
 
+	@Override
+	public void showClassName() {
+		System.out.println("I am from abstract class");		
 	}
 
 }
 
+class A1 extends A2{
 
+	@Override
+	public void getSalary() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getMoney() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+//eger ki bir class içerisinde abstract bir metod tanýmlamak istiyor iseniz o class da abstrac olmak
+//zorundadýr. yoksa o class içirisinde abstrac bir metod tanýmlanamaz.
+abstract class A2{
+	
+	final int MAX_SALARY = 45000;
+	public abstract void getSalary();
+	public abstract void getMoney();
+	
+}
+
+final class A3{
+	
+	 private static String NAME = "MUHAMMET";
+	 
+}
