13a9eed7f39155f4a92fe9e86a87df1f00f1c750
muhammetaslan <muhammet.aslan1995@gmail.com>
2018-02-12 23:59:54 +0300

    Interface declaration and IntrefaceTest.class

---
+++ src/muhammetaslan/javase/interface_declaration/InterfaceTest.java
@@ -0,0 +1,36 @@
+package muhammetaslan.javase.interface_declaration;
+
+public class InterfaceTest {
+
+	public static void main(String[] args) {
+		
+		A a_obj = new A();
+		B b_obj = new B();
+		
+		a_obj.run();
+	}
+
+}
+
+
+// Javada runnable diye bir arabirim vardýr 
+class A implements Runnable{
+
+	@Override
+	public void run() {
+		
+		System.out.println("Class A is run");
+		
+	}
+	
+}
+
+class B implements Runnable{
+
+	@Override
+	public void run() {
+		System.out.println("Class B is run");
+		
+	}
+	
+}
