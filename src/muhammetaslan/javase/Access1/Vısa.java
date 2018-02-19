7f0cf63d597f69415613f04ae9f2dd88af5a44bf
muhammetaslan <muhammet.aslan1995@gmail.com>
2018-02-19 23:35:54 +0300

    Vısa.java test with access to Vısa

--- src/muhammetaslan/javase/Access1/Vısa.java
+++ src/muhammetaslan/javase/Access1/Vısa.java
@@ -1,11 +1,21 @@
 package muhammetaslan.javase.Access1;
 
+import java.util.Date;
+
 //Výsa sinýfý olarak Card sýnýfýna extend ediliyor fakat private deðiþken ve methodlar hala invisible
 
 public class Výsa {
 
-	private void mai() {
-		// TODO Auto-generated method stub
-
+	private void main() {
+		
+		Card card1 = new Card();
+		
+		card1.setCardNumber(121215);
+		card1.setUsageDate(new Date(2020,02,15));
+		// Card . custerName access level is default - packeage level bu yüzden ulaþabildik  
+		String a = card1.custerName;
+		
+		System.out.println("Security number access level is protected" + card1.securityNumber);
+		System.out.println("Usage Data is and access level is public" + card1.usageDate);
 	}
 }
