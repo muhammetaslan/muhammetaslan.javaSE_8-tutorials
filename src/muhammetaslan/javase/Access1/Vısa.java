ac153f90c93387657a3f16fbf8424c2c4b4bd65c
muhammetaslan <muhammet.aslan1995@gmail.com>
2018-02-19 23:22:51 +0300

    modified

--- src/muhammetaslan/javase/Access1/Vısa.java
+++ src/muhammetaslan/javase/Access1/Vısa.java
@@ -1,5 +1,11 @@
 package muhammetaslan.javase.Access1;
 
+//Výsa sinýfý olarak Card sýnýfýna extend ediliyor fakat private deðiþken ve methodlar hala invisible
+
 public class Výsa {
 
+	private void mai() {
+		// TODO Auto-generated method stub
+
+	}
 }
